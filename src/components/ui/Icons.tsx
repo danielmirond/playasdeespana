c66ecdd125ed45b982c9daff7d5771be3d60b744
danielmirond <tu@email.com>
2026-03-10 20:56:28 +0100

    fix: añadir Umbrella a Icons.tsx

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -1,5 +1,6 @@
 'use client'
 export { 
   Camera, Waves, Sun, Drop, ForkKnife, Bed, Thermometer, Wind, 
-  Car, Bus, Bicycle, Person, MapPin, Star, Users, Park, PersonSimpleSwim
+  Car, Bus, Bicycle, Person, MapPin, Star, Users, Park, 
+  PersonSimpleSwim, Umbrella
 } from '@phosphor-icons/react'
